dac72f0b4be729472626744bb23f4b74a939fd70
Sahana NR <saanunr99>
2024-06-25 06:14:01 +0530

    This is my first commit

---
+++ Exam.java
@@ -0,0 +1,17 @@
+public class Exam {
+    public static void main(String[] args) {
+        // Declare subject marks
+        int subject1 = 80;
+        int subject2 = 99;
+        int subject3 = 76;
+        int subject4 = 82;
+        int subject5 = 78;
+        int subject6 = 73;
+
+        // Calculate total score
+        int totalScore = subject1 + subject2 + subject3 + subject4 + subject5 + subject6;
+
+        // Display total score
+        System.out.println("Total Score: " + totalScore);
+    }
+}
