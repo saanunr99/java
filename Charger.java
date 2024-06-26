d9fe3df5ca4400033c3cb215f4d6627d410202d8
Sahana NR <saanunr99>
2024-06-26 21:59:40 +0530

    21/6

---
+++ Charger.java
@@ -0,0 +1,19 @@
+public class Charger {
+ 
+	
+		 public static String brand = "Anker";
+		 public static String model = "PowerPort Atom III";
+		 public static int maxOutputWatts = 60;
+		 public static boolean isFastChargeSupported = true;
+		 public static String compatibleDevices = "iPhone Android phones tablets";
+		 public static String connectorType = "USB-C";
+		 public static boolean isPortable = true;
+		 public static String color = "Black";
+		 public static double cableLengthMeters = 1.5;
+		 public static boolean isSmartCharger = true;
+		
+    
+ 
+        
+   
+}
