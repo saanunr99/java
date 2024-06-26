bafa91cf8a83f43e8b183cb23e050d9c505181a4
Sahana NR <saanunr99>
2024-06-26 21:53:32 +0530

    21/6

---
+++ Chair.java
@@ -0,0 +1,17 @@
+public class Chair {
+    // Static variables
+	
+		 public static String material = "Wood";
+		 public static int numberOfLegs = 4;
+		 public static boolean isAdjustable = true;
+		 public static String color = "Brown";
+		 public static double height = 90.5;
+		 public static boolean hasBackrest = true;
+		 public static String manufacturer = "Furniture Co.";
+		 public static boolean isFoldable = false;
+		
+    
+ 
+        
+   
+}
