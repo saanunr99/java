ce32cba03dadda8100491ff812d5ed49288ca4d8
Sahana NR <saanunr99>
2024-06-26 21:56:53 +0530

    21/6

---
+++ Chair1.java
@@ -0,0 +1,23 @@
+public class Chair1 {
+	public static void main(String[] args){
+	
+		System.out.println("Chair Material: " +Chair. material);
+        System.out.println("Number of Legs: " + Chair.numberOfLegs);
+        System.out.println("Is Adjustable: " + Chair.isAdjustable);
+        System.out.println("Color: " + Chair.color);
+        System.out.println("Height: " + Chair.height + " cm");
+        System.out.println("Has Backrest: " + Chair.hasBackrest);
+        System.out.println("Manufacturer: " + Chair.manufacturer);
+        System.out.println("Is Foldable: " + Chair.isFoldable );
+	 
+	 
+
+        
+      
+	
+	
+	}
+
+
+
+}
