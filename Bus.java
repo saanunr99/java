ca75ec52d5f6fdfcae10c41b901c322923d91ac3
Sahana NR <saanunr99>
2024-06-26 21:44:51 +0530

    20/6

---
+++ Bus.java
@@ -0,0 +1,34 @@
+public class Bus {
+    public static void main(String[] args) {
+        
+        int Bus1 = 175;
+        int Bus2 = 180;
+        int Bus3 = 648;
+        int Bus4 = 677;
+        int Bus5 = 295;
+        int Bus6 = 1295;
+        int Bus7 = 175;
+        int Bus8 = 295;
+		int Bus9 = 710;
+		int Bus10 = 695;
+		if(Bus2<Bus9){
+			System.out.println("Bus2"+"Nellore to Sullurupeta"+Bus2);
+		
+		
+		}else{
+			System.out.println("Bus9"+"NelloretoAhmedabad"+Bus9);
+		}
+
+        System.out.println("Bus1 cost is:"+Bus1);
+		System.out.println("Bus2 cost is:"+Bus2);
+		System.out.println("Bus3 cost is:"+Bus3);
+		System.out.println("Bus4 cost is:"+Bus4);
+		System.out.println("Bus5 cost is:"+Bus5);
+		System.out.println("Bus6 cost is:"+Bus6);
+		System.out.println("Bus7 cost is:"+Bus7);
+		System.out.println("Bus8 cost is:"+Bus8);
+		System.out.println("Bus9 cost is:"+Bus9);
+		System.out.println("Bus10 cost is:"+Bus10);
+        
+    }
+}
