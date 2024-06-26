e00ad61384713cc6426a601e8ad6169adbcacb30
Sahana NR <saanunr99>
2024-06-26 21:50:10 +0530

    19/6

---
+++ AgeCheck.java
@@ -0,0 +1,15 @@
+public class AgeCheck{
+
+   public static void main(String args[]){
+   
+   int age = 18;
+   int sahanaAge = 23;
+   if(age > SahanaAge) {
+	   
+      System.out.println("condition is true");
+   } 
+     else{
+	   System.out.println("condition is false");
+	   }
+	   }
+ }
