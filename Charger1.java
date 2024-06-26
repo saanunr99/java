e2b28edf21631ff6408aa49daa9bc0118a7e785f
Sahana NR <saanunr99>
2024-06-26 22:02:20 +0530

    21/6

---
+++ Charger1.java
@@ -0,0 +1,25 @@
+public class Charger1 {
+	public static void main(String[] args){
+	
+		System.out.println("Charger Brand: " +Charger. brand);
+        System.out.println("Charger Model: " + Charger.model);
+        System.out.println("Max Output Watts: " +Charger. maxOutputWatts);
+        System.out.println("Fast Charge Supported: " + Charger.isFastChargeSupported);
+        System.out.println("Compatible Devices: " +Charger. compatibleDevices);
+        System.out.println("Connector Type: " + Charger.connectorType);
+        System.out.println("Is Portable: " + Charger.isPortable );
+        System.out.println("Color: " +Charger. color);
+        System.out.println("Cable Length: " + Charger.cableLengthMeters + " meters");
+        System.out.println("Is Smart Charger: " + Charger.isSmartCharger );
+	 
+	 
+
+        
+      
+	
+	
+	}
+
+
+
+}
