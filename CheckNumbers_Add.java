06a68cb39e22481a888fb9e62c5c783a40af6f7f
Sahana NR <saanunr99>
2024-06-26 21:08:10 +0530

    Tuesday assignment

---
+++ CheckNumbers_Add.java
@@ -0,0 +1,18 @@
+public class CheckNumbers_Add {
+    public static void main(String[] args) {
+    
+        int num1 = 36;
+	    int num2 = 36;
+		int sum = num1 + num2;
+        System.out.println("Sum Of Two Numbers Is " +sum);
+
+        
+        if (sum == num1) {
+            System.out.printf("The sum of two numbers is equal to the first number");
+        } else {
+            System.out.printf("The sum of two numbers is not equal to the first number ");
+        }
+
+        
+    }
+}
