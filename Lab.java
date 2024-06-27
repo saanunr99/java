d72b073c59d9835ed7a2ca9a4653562151cb5627
Sahana NR <saanunr99>
2024-06-27 21:47:29 +0530

    21/6

---
+++ Lab.java
@@ -0,0 +1,14 @@
+public class Lab {
+	
+		 public static String labName = "Computer Science Lab";
+		 public static int numberOfComputers = 20;
+		 public static boolean isOpen = true;
+		 public static String softwareInstalled = "Java JDK";
+		 public static double averageTemperature = 24.5;
+		 public static String labSupervisor = "Dr. Smith";
+		
+    
+ 
+        
+   
+}
