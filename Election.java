ba9e701bf305464ef8da95ac1fd8f886b0c66631
Sahana NR <saanunr99>
2024-06-27 21:30:25 +0530

    21/6

---
+++ Election.java
@@ -0,0 +1,14 @@
+public class Election {
+	
+        public static String name;
+		public static int totalvotes;
+        public static boolean isElectionday;
+        public static char leadingparty;
+        public static float voterTurnoutPercentage;
+        public static double campingFunds;  
+         
+        
+		
+        
+    
+}
