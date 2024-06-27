c4e04fe0979a3aaf38d0843c25fe4db5817f2815
Sahana NR <saanunr99>
2024-06-27 21:28:04 +0530

    19/6

---
+++ door.java
@@ -0,0 +1,19 @@
+public class Door{
+public static void main(String[] args){
+System.out.println("Displaying a water Level in a tank");
+int weight = 1100;
+float thickness = 5;
+double inches = 4.5;
+boolean door = false ;
+byte doors= 1;
+long height = 7;
+short size = 6;
+System.out.println("weight is:"+weight);
+System.out.println("thickness  is:"+thickness );
+System.out.println("inches  is:"+inches);
+System.out.println(" door:"+ door);
+System.out.println("doors :"+doors);
+System.out.println("height is:"+height);
+System.out.println("size is:"+size);
+}
+}
