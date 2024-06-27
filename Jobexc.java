bce6e38da088bdbda804a11f277287fc8d7fb826
Sahana NR <saanunr99>
2024-06-27 21:44:49 +0530

    19/6

---
+++ Jobexc.java
@@ -0,0 +1,41 @@
+public class Jobexc{
+	public static void main(String args[]){
+	
+	
+		String comapany = "IBM";
+		String jobRole = "software developer";
+		String joiningyear = "2024";
+		String skills = "java fullstack";
+		String ctc = "9 lpa";
+		String qualification = "BE";
+		String experience = "fresher";
+		String languages = "English,Kannada";
+		String citizen = "Indian";
+		String location = "Benagluru";
+		String projects = "web Application development";
+		String courses = "java fullstck";
+		String interns = "software intern";
+		String shift = "night";
+		String workingHours = "8 hours";
+	
+	
+	System.out.println("comapany:"+comapany);
+	System.out.println("jobRole :"+jobRole );
+	System.out.println("joiningyear :"+joiningyear);
+	System.out.println("skills :"+skills);
+	System.out.println("ctc:"+ ctc);
+	System.out.println("qualification:"+qualification);
+	System.out.println("experience:"+experience);
+	System.out.println("languages:"+ languages);
+	System.out.println("citizen:"+ citizen);
+	System.out.println("location:"+ location );
+	System.out.println("projects:"+ projects);
+	System.out.println("courses :"+courses);
+	System.out.println("interns :"+interns);
+	System.out.println("shift:"+ shift);
+	System.out.println("workingHours :"+workingHours);
+													
+
+	}
+
+}
