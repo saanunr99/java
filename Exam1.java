29d1c432fb4ce2a81f1c56abd0c0e0a4093bc2ab
Sahana NR <saanunr99>
2024-06-27 21:36:25 +0530

    22/6

---
+++ Exam1.java
@@ -0,0 +1,16 @@
+public class Exam1 {
+  public static void main(String[] args){
+    
+     int kannada = 99;
+     int english = 98;
+     int hindi = 95;
+	 int science = 80;
+	 int social = 85;
+	 int maths = 89;
+	 
+	 int Totalscore = kannada+english+hindi+science+social+maths;
+	 
+	 System.out.println("Total score:" + Totalscore);
+	 
+  }
+}
