fe3c84415e19352a53aa268ed275e82ff675160a
Sahana NR <saanunr99>
2024-06-27 21:33:55 +0530

    21/6

---
+++ Election1.java
@@ -0,0 +1,13 @@
+class Election1{
+	public static void main(String[] args){
+	
+        System.out.println("String name:" +Election.name);
+        System.out.println("totalvotes:" + Election.totalvotes );
+        System.out.println("isElectionday:" +Election.isElectionday);
+        System.out.println("leadingparty:" + Election.leadingparty );
+        System.out.println("voterTurnoutPercentage:" + Election.voterTurnoutPercentage);
+        System.out.println("campingFunds:" + Election.campingFunds );
+
+
+	}
+}
