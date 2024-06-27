826ed251863dc072963e43d0ad2ed721a47a6f49
Sahana NR <saanunr99>
2024-06-27 21:42:21 +0530

    18/6

---
+++ Instagramacc.java
@@ -0,0 +1,12 @@
+public class Instagramacc{
+ public static void main(String[] args){
+
+    System.out.println("Local variable");
+    String userId = "xworkzbtm_01";
+    String password = "xworkzbtm_01";
+    System.out.println("user id is : "+userId);
+    System.out.println("password is : "+password);
+	
+	}
+	
+	}
