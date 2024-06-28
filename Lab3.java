440ef07817e7a8fc0e78146b37764b7c935e3892
Sahana NR <saanunr99>
2024-06-28 09:58:32 +0530

    21/6

---
+++ Lab3.java
@@ -0,0 +1,20 @@
+public class Lab3 {
+	public static void main(String[] args){
+	
+	 System.out.println(" labName"+Subject3.labName );
+     System.out.println("numberOfComputers "+Subject3.numberOfComputers);
+	 System.out.println("isOpen"+Subject3.isOpen);
+	 System.out.println("softwareInstalled"+Subject3.softwareInstalled);
+	 System.out.println("labSupervisor"+Subject3.labSupervisor);
+	 
+	 
+
+        
+      
+	
+	
+	}
+
+
+
+}
