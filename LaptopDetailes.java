010c2310adb3348f84701e8630af29c18ab42d32
Sahana NR <saanunr99>
2024-06-28 10:04:46 +0530

    20/6

---
+++ LaptopDetailes.java
@@ -0,0 +1,13 @@
+Public class LaptopDetailes{
+Public static void main(string arg[])'
+ public static int laptopGen;
+ public static double laptopprice;
+ public static double laptopweight;
+ public static booliean laptoprating;
+ public  static float laptopdisplay;
+System.out.println(Laptopdetailes Laptopdetailesrun)
+
+
+
+
+}
