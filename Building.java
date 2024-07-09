a66799908c8daf263efba3330598bfe90374cac3
Sahana NR <saanunr99>
2024-07-09 07:56:19 +0530

    task

---
+++ Building.java
@@ -0,0 +1,15 @@
+public class Building {
+ public void construct() {
+ System.out.println("Building a tall building");
+ } 
+ 
+ public void construct(String material){
+ 
+ System.out.println("Building:" + material, building);
+ }
+ public void construct (String material, int floors){
+ 
+ System.out.println("Buildings:" + material, building, "floors:" + "floors");
+ 
+ } 
+ }
---
+++ BuildingRunner.java
@@ -0,0 +1,8 @@
+public class BuildingRunner {
+  public static void main(Strin[] args){
+  Building building = new Building();
+  building.construct();
+  building.construct("brick");
+  building.construct("steel", 5)
+  }
+}
