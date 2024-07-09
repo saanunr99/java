3e40d20f3c513e4a717148aab745ca579d6c4f95
Sahana NR <saanunr99>
2024-07-09 07:35:13 +0530

    task

---
+++ Traffic9.java
@@ -0,0 +1,15 @@
+public class Traffic9{
+ public void sound(){
+ System.out.println("no sound");
+ }
+ 
+public void sound(String sound){
+ system.out.println(sound );
+ } 
+  public void sound(String sound, int frequency)
+  {
+  System.out.println(sound + "frequency:" +frequency);
+  }
+ }
+ 
+ 
---
+++ Traffic9Runner.java
@@ -0,0 +1,8 @@
+public class Traffic9Runner{
+ public static void main(String[] args){
+    Traffic traffic= new traffic();
+	traffic.sound();
+	traffic.sound("Beep");
+	traffic.sound("Beep", 200);
+	}
+}
