380f17878d3b0b39acdd7c3da328ccd4468c8a27
Sahana NR <saanunr99>
2024-07-09 08:35:08 +0530

    task

---
+++ BloodGroup.java
@@ -0,0 +1,16 @@
+public class BloodGroup{
+public void printbloodgroup(){
+ 
+ System.out.println("Unknown blood group");
+ }
+ 
+ public void printbloodgroup(String bloodgroup){
+ 
+ System.out.println("Blood group:"+ bloodgroup);
+ }
+ 
+ public void printBloodGroup(String bloodgroup, String rhtype){
+ 
+ System.out.println("Blood group:" + bloodgroup+ "A" +rhtype );
+ }
+ }
---
+++ BloodGroupRunner.java
@@ -0,0 +1,8 @@
+public class BloodGroupRunner{
+public static void main(String[] args){
+  BloodGroup bloodgroup= new bloodgroup();
+  bloodgroup.printbloodgroup();
+  bloodgroup.printbloodgroup("A");
+  bloodgroup.printbloodgroup("B", "positive");
+  }
+  }
