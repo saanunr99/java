3e87a87f0e18b0f7534e805ab3c40e13bf4ac477
Sahana NR <saanunr99>
2024-07-09 08:22:07 +0530

    task

---
+++ OLX.java
@@ -0,0 +1,15 @@
+public class OLX{
+public void postad(){
+
+System.out.println("posting a ads on olx");
+}
+  public void postad(String title){
+  
+  System.out.println("posting ads with title:" +title);
+  }
+  
+  public void postad(String title, String description){
+  System.out.println("posting ads with title:"+ title , description);
+  
+  }
+  }
---
+++ OLXRunner.java
@@ -0,0 +1,8 @@
+public class OLXRunner{
+ public static void main(String[] args){
+   OLX olx = new OLX();
+   olx.postad();
+   olx.postad("oppophone for sale");
+   olx.postad("oppphone for sale:"+" Excellent condition, 150GB");
+ }
+}
