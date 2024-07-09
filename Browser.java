8cb77fe02632823c60521e3158559c5e992b2d15
Sahana NR <saanunr99>
2024-07-09 08:48:19 +0530

    task

---
+++ Browser.java
@@ -0,0 +1,15 @@
+public class Browser{
+ public void open(){
+  
+ System.out.println("opening link in browser");
+ }
+ 
+ public void open(String url)
+ {
+ System.out.println("opening" + url + " browser");
+ }
+ 
+ public void open(String url, String browsertype){
+ System.out.println ("opening:" url + "browser:"+browser);
+ }
+ }
---
+++ BrowserRunner.java
@@ -0,0 +1,9 @@
+public class BrowserRunner{
+ public static void main(String[] args){
+  Browser browser=new Browser();
+  browser.open();
+  browser.open("(link unavailable)");
+   browser.open("link unavailable") , "chrome"); 
+   }
+   
+}
