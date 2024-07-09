4729a66353f71b32d9953d9a38f50ce89a342eff
Sahana NR <saanunr99>
2024-07-09 08:07:20 +0530

    task

---
+++ Disney.java
@@ -0,0 +1,16 @@
+public class Disney{
+ public void welcom(){
+ 
+ System.out.println("welcome to disney");
+ }
+ 
+  public void welcome (String name){
+    
+	System.out.println("welcome:" + name "disney");
+	}
+	
+	public void welcome (String name, int age ){
+	
+	System.out.println("welcome, name:" age+ years old to disney);
+	}
+	}
---
+++ DisneyRunner.java
@@ -0,0 +1,6 @@
+public class DisneyRunner {
+ public static void main(Strin[] args ){
+  Disney.welcome();
+  disney.welcome();
+  disney.welcome("mickey");
+  disney.welcome("doremon",20)}
