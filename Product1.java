e76beda30b334f2435bd02ea17dcbf73d793d3d1
Sahana NR <saanunr99>
2024-07-09 07:38:54 +0530

    task

---
+++ Product1.java
@@ -0,0 +1,9 @@
+public class Product1{
+ public static void product( String productname){
+ System.out.println("Productname:" +productname);
+ }
+
+ public static void productdetailes( float productratings){
+ System.out.println ("productdetailes is:" +productname, productdetailes);
+ }
+}
---
+++ Product1Runner.java
@@ -0,0 +1,11 @@
+public class Product1Runner{
+public static void main( string[] args){
+
+   Product.product(cloths);
+   Product.details(4.5f);
+   Product.cloth("big", "small");
+   
+   system.out.println(Products. Productdetails);
+   
+}
+}
