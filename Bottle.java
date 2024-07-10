528ffbcd5dcef15febe7eb51d93bd0751cbf0623
Sahana NR <saanunr99>
2024-07-10 22:01:38 +0530

    task

---
+++ Bottle.java
@@ -0,0 +1,57 @@
+public class Bottle {
+   public static int var1;
+   public static int var2;
+   public static int var3;
+   public static int var4;
+   public static int var5;
+   public static int var6;
+   public static int var7;
+   public static int var8;
+   public static int var9;
+   public static int var10;
+   
+   
+   public static void assignvalues() {
+       var1=52;
+	   var2=30;
+       var3=47;
+       var4=74;
+	   var5=96;
+	   var6=85;
+	   var7=52;
+	   var8=74;
+	   var9=85;
+	   var10=47;
+	   
+	   public Static void main(String[] args){
+	       assignvalues();
+		   
+		   System.out.println("Values:");
+		         System.out.println("var1=" + var1);
+		         System.out.println("var2=" + var2);
+		         System.out.println("var3=" + var3);
+		         System.out.println("var4=" + var4);
+		         System.out.println("var5=" + var5);
+		         System.out.println("var6=" + var6);
+		         System.out.println("var7=" + var7);
+		         System.out.println("var8=" + var8);
+		         System.out.println("var9=" + var9);
+		         System.out.println("var10=" + var10);
+
+				 }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
