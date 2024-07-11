f3c61adc2ec21f25d3ff4367bc4d5e69064ff148
Sahana NR <saanunr99>
2024-07-11 07:02:44 +0530

    task

---
+++ Youtube.java
@@ -0,0 +1,40 @@
+public class Youtube{
+
+ static int followers;
+ static String userprofile;
+ static int followers;
+ static String loginid;
+ static long viwers;
+ static String password;
+ static long phonenumber;
+ static boolean useractive;
+ static float apprating;
+ static int posts;
+ static char grade;
+ static double likes;
+ 
+ public static void aboutyoutube(){
+ followers = 3000;
+ username = "sahana";
+ following = 100;
+ viwers = "saanu_123";
+ password="saanu123";
+ phonenumber=8563829275L;
+ userActive=true;
+ appratings= 9.5f;
+ posts=5;
+ grade='A';
+ likes=4.5;
+ }
+ 
+ public static void main(String args[]){
+ System.out.println("total followers:"+followers);
+ System.out.println("user:"+username);
+  System.out.println("total following:"+followings);
+    System.out.println("total viwers:"+viwers);
+  System.out.println("total loginid:"+loginid);
+ System.out.println("total password:"+password);
+ System.out.println("active:"+useractive);
+ System.out.println("total appratings:"+appratings);
+}
+}
